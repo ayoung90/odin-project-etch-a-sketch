a7d8bc32fb19ece22026a2a22b7c57cf938a7e4f
ayoung90 <ayoung90@live.com.au>
2023-05-21 21:19:50 +1000

    Add the ability to define the grid size

--- js/game.js
+++ js/game.js
@@ -1,5 +1,6 @@
-const boardHeight = 16;
-const boardWidth = 16;
+const maxHeightWidth = 100;
+let boardHeight = 16;
+let boardWidth = 16;
 
 //styles
 const cellClass = "cell";
@@ -9,6 +10,9 @@ const hoveredClass = "hovered";
 function renderBoard() {
   let canvas = document.querySelector("#canvasContainer");
 
+  // clear canvas if not already empty
+  canvas.innerHTML = null;
+
   for (i = 0; i < boardHeight; i++) {
     //create a div per row
     let row = document.createElement("div");
@@ -17,10 +21,48 @@ function renderBoard() {
       //create a div per column
       let column = document.createElement("div");
       column.classList.add(cellClass);
+      column.addEventListener("mouseenter", changeColor);
       row.appendChild(column);
     }
 
     canvas.appendChild(row);
+  }
+}
+function setGridSize() {
+  let reRender = false;
+  let heightInput = window.prompt(
+    `Please define board height (under ${maxHeightWidth}):`,
+    boardHeight
+  );
+
+  if (!heightInput || isNaN(heightInput)) {
+  } else if (heightInput > maxHeightWidth) {
+    window.alert(
+      `Height must be under ${maxHeightWidth}, you entered ${heightInput}`
+    );
+  } else {
+    boardHeight = heightInput;
+    reRender = true;
+  }
+
+  let widthInput = window.prompt(
+    `Please define board width (under ${maxHeightWidth}):`,
+    boardWidth
+  );
+
+  if (!widthInput || isNaN(widthInput)) {
+  } else if (widthInput > maxHeightWidth) {
+    window.alert(
+      `Width must be under ${maxHeightWidth}, you entered ${widthInput}`
+    );
+  } else {
+    boardWidth = widthInput;
+    reRender = true;
+  }
+
+  //re-render board with new attributes if one has changes
+  if (reRender) {
+    renderBoard();
   }
 }
 
@@ -36,11 +78,8 @@ function resetBoard() {
 
 renderBoard();
 
-let cells = document.querySelectorAll(`.${cellClass}`);
-
-cells.forEach((cell) => {
-  cell.addEventListener("mouseenter", changeColor);
-});
-
-let resetButton = document.querySelector("#reset");
+const resetButton = document.querySelector("#reset");
 resetButton.addEventListener("click", resetBoard);
+
+const gridSizeButton = document.querySelector("#gridSize");
+gridSizeButton.addEventListener("click", setGridSize);
