92d7b5e413d47b1a2394ed89e0fa76c0d8ea5bae
ayoung90 <ayoung90@live.com.au>
2023-05-21 21:25:51 +1000

    Complete project. Add documentation

--- js/game.js
+++ js/game.js
@@ -1,12 +1,20 @@
-const maxHeightWidth = 100;
-let boardHeight = 16;
-let boardWidth = 16;
+// constants
+const MAX_HEIGHT_WIDTH = 100;
+const DEFAULT_HEIGHT = 16;
+const DEFAULT_WIDTH = 16;
+
+let boardHeight = DEFAULT_HEIGHT;
+let boardWidth = DEFAULT_WIDTH;
 
 //styles
 const cellClass = "cell";
 const rowClass = "row";
 const hoveredClass = "hovered";
 
+/**
+ * Creates a board based on the defined height and width.
+ * Resets the board if it has already been created
+ */
 function renderBoard() {
   let canvas = document.querySelector("#canvasContainer");
 
@@ -28,17 +36,21 @@ function renderBoard() {
     canvas.appendChild(row);
   }
 }
+/**
+ * Takes user input to define the height and width of the board
+ * if either have been set, re-renders with the new attributes
+ */
 function setGridSize() {
   let reRender = false;
   let heightInput = window.prompt(
-    `Please define board height (under ${maxHeightWidth}):`,
+    `Please define board height (under ${MAX_HEIGHT_WIDTH}):`,
     boardHeight
   );
 
   if (!heightInput || isNaN(heightInput)) {
-  } else if (heightInput > maxHeightWidth) {
+  } else if (heightInput > MAX_HEIGHT_WIDTH) {
     window.alert(
-      `Height must be under ${maxHeightWidth}, you entered ${heightInput}`
+      `Height must be under ${MAX_HEIGHT_WIDTH}, you entered ${heightInput}`
     );
   } else {
     boardHeight = heightInput;
@@ -46,14 +58,14 @@ function setGridSize() {
   }
 
   let widthInput = window.prompt(
-    `Please define board width (under ${maxHeightWidth}):`,
+    `Please define board width (under ${MAX_HEIGHT_WIDTH}):`,
     boardWidth
   );
 
   if (!widthInput || isNaN(widthInput)) {
-  } else if (widthInput > maxHeightWidth) {
+  } else if (widthInput > MAX_HEIGHT_WIDTH) {
     window.alert(
-      `Width must be under ${maxHeightWidth}, you entered ${widthInput}`
+      `Width must be under ${MAX_HEIGHT_WIDTH}, you entered ${widthInput}`
     );
   } else {
     boardWidth = widthInput;
@@ -66,9 +78,16 @@ function setGridSize() {
   }
 }
 
+/**
+ * Sets the hovered class to change the colour of the cell
+ * @param {*} event
+ */
 function changeColor(event) {
   event.target.classList.add(hoveredClass);
 }
+/**
+ * resets all cells to default colour by removing the hovered class
+ */
 function resetBoard() {
   let cellsToReset = document.querySelectorAll(`.${hoveredClass}`);
   cellsToReset.forEach((cell) => {
