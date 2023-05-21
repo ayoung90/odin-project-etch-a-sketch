78e437abd576b91ae9067a15e681bc67d352ad2d
ayoung90 <ayoung90@live.com.au>
2023-05-21 20:47:45 +1000

    Hover changes cells to grey. Allow user to reset grid to white

--- js/game.js
+++ js/game.js
@@ -1,17 +1,22 @@
 const boardHeight = 16;
 const boardWidth = 16;
 
+//styles
+const cellClass = "cell";
+const rowClass = "row";
+const hoveredClass = "hovered";
+
 function renderBoard() {
   let canvas = document.querySelector("#canvasContainer");
 
   for (i = 0; i < boardHeight; i++) {
     //create a div per row
     let row = document.createElement("div");
-    row.classList.add("row");
+    row.classList.add(rowClass);
     for (j = 0; j < boardWidth; j++) {
       //create a div per column
       let column = document.createElement("div");
-      column.classList.add("cell");
+      column.classList.add(cellClass);
       row.appendChild(column);
     }
 
@@ -19,4 +24,23 @@ function renderBoard() {
   }
 }
 
+function changeColor(event) {
+  event.target.classList.add(hoveredClass);
+}
+function resetBoard() {
+  let cellsToReset = document.querySelectorAll(`.${hoveredClass}`);
+  cellsToReset.forEach((cell) => {
+    cell.classList.remove(hoveredClass);
+  });
+}
+
 renderBoard();
+
+let cells = document.querySelectorAll(`.${cellClass}`);
+
+cells.forEach((cell) => {
+  cell.addEventListener("mouseenter", changeColor);
+});
+
+let resetButton = document.querySelector("#reset");
+resetButton.addEventListener("click", resetBoard);
